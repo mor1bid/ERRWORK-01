045c070929fcca73042ee390aecf50eba5f2de3c
De <spark49942@gmail.com>
2023-04-13 05:37:55 +0300

    errorwork1-wt

--- dz.java
+++ dz.java
@@ -3,12 +3,25 @@ import java.util.*;
 public class dz {
     public static void main(String[] args) 
     {
+        boolean acceptable = false;
         Scanner work = new Scanner(System.in);
         System.out.println("Введите размер первого массива:");
         int mylen1 = work.nextInt();
         System.out.println("Введите размер второго массива:");
         int mylen2 = work.nextInt();
-
+        if (mylen1 == mylen2) 
+        {
+            acceptable = true;
+        }
+        else 
+        {
+            System.out.println("Данные массивы не совпадают по длине! Не все элементы буду задействованы в работе. Продолжить? 1/0");
+            int choi = work.nextInt();
+            if (choi == 1) 
+            {
+                acceptable = true;
+            }
+        }
         Integer ar[] = new Integer[mylen1];
         Integer ray[] = new Integer[mylen2];
         Random rnd = new Random();
@@ -24,28 +37,60 @@ public class dz {
 
         System.out.println(Arrays.asList(ar) + "\n");
         System.out.println(Arrays.asList(ray));
-        massCollide(ar, ray);
-
+        if (acceptable) 
+        {
+            massCollide(ar, ray);
+            massDivide(ar, ray);
+        }
         work.close();
     }
 
+    // 1. Реализуйте метод, принимающий в качестве аргументов два целочисленных массива, 
+    // и возвращающий новый массив, каждый элемент которого равен разности элементов двух входящих массивов в той
+    // же ячейке. Если длины массивов не равны, необходимо как-то оповестить пользователя.
+    
     public static void massCollide(Integer ar[], Integer ray[]) 
     {
-        int masslen;
+        int len = ar.length;
         if (ar.length > ray.length) 
         {
-            masslen = ar.length;
-            System.out.println("Предупреждение - данные массивы несовпадают по длине! Не все элементы буду задействованы в работе.");
+            len = ray.length;
         }
-        else 
-        {
-            masslen = ray.length;
-        }
-        Integer array[] = new Integer[masslen];
-        for (int i = 0; i < masslen; i++) 
+        Integer array[] = new Integer[len];
+        for (int i = 0; i < len; i++) 
         {
             array[i] = ar[i] - ray[i];
         }
-        System.out.println(Arrays.asList(array));
+        System.out.println("\nРезультат: " + Arrays.asList(array));
+    }
+
+    // 2. Реализуйте метод, принимающий в качестве аргументов два целочисленных массива, 
+    // и возвращающий новый массив, каждый элемент которого равен частному элементов двух входящих массивов в той
+    // же ячейке. Если длины массивов не равны, необходимо как-то оповестить пользователя.
+    // Важно: При выполнении метода единственное исключение, которое пользователь может увидеть -
+    // RuntimeException, т.е. ваше.
+    
+    public static void massDivide(Integer ar[], Integer ray[]) 
+    {
+        int len = ar.length;
+        if (ar.length > ray.length) 
+        {
+            len = ray.length;
+        }
+        double array[] = new double[len];
+        for (int i = 0; i < len; i++) 
+        {
+            double arg1 = (double) ar[i];
+            double arg2 = (double) ray[i];
+            try 
+            {
+                array[i] = arg1 / arg2;
+            } 
+            catch (Exception e) 
+            {
+                throw new RuntimeException("Деление на ноль!");
+            }
+        }
+        System.out.println("\nРезультат: " + Arrays.asList(array));
     }
 }
